4f6ce5a893944f6b7939c8adb4f1bdc82cc26557
jargyl <jari.gielen@gmail.com>
2023-01-04 20:06:17 +0100

    Added whitespace under image with product info

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import json
 from PIL import Image, ImageDraw, ImageFont
 
 
-def get_product_picture(query):
+def get_product_picture(query, quantity):
     r = requests.get(f'https://www.ah.be/zoeken/api/products/search?query={query}&size=1')
     data = json.loads(r.text)
     product = data['cards'][0]['products'][0]
-    product_url = f"https://www.ah.be{product['link']}"
     image_url = product['images'][1]['url']
 
     filename = query.replace(' ', '-').replace('"', '')
@@ -22,14 +21,18 @@ def get_product_picture(query):
 
         im = Image.open("images/" + filename + ".png").convert('RGBA')
 
-        draw = ImageDraw.Draw(im)
-
-        font = ImageFont.truetype("arial.ttf", size=20)
-
         width, height = im.size
 
-        max_width = width - 20
-        font_size = 16
+        new_height = height + 65
+        new_im = Image.new('RGB', (width, new_height), (230, 230, 230))
+
+        new_im.paste(im, (0, 0))
+
+        draw = ImageDraw.Draw(new_im)
+        font = ImageFont.truetype("arial.ttf", size=30)
+
+        max_width = width
+        font_size = 30
         while True:
             text_width, text_height = draw.textsize(query, font=font)
             if text_width <= max_width:
@@ -38,18 +41,10 @@ def get_product_picture(query):
             font = ImageFont.truetype('arial.ttf', font_size)
 
         x = (width - text_width) // 2
-
-        rect_y2 = height
-        y = rect_y2 - text_height
-
-        # Draw a rectangle behind the text
-        rect_x1 = x - 5  # Add some padding around the text
-        rect_y1 = y - 5
-        rect_x2 = x + text_width + 5
-        draw.rectangle((rect_x1, rect_y1, rect_x2, rect_y2), fill=(255, 255, 255))
+        y = height + 15
 
         draw.text((x, y), query, (0, 0, 0), font=font)
-        im.save("images/" + filename + ".png")
+        new_im.save("images/" + filename + ".png")
 
 
-get_product_picture("Douwe Egberts Lungo original capsules voordeelpak(20 stuks)")
+get_product_picture("AH Volle melk(0,5 l)")
